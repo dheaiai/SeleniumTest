9043e2f6534a5f1f0d5602236ae866398c570141
dheaiai <dheerajjain60@gmail.com>
2023-07-07 21:38:17 +0200

    Logging functionality implemented

--- 50_Most_Important/test_check_login.py
+++ 50_Most_Important/test_check_login.py
@@ -17,26 +17,21 @@ from selenium.webdriver.support import expected_conditions as EC
 class Verifyloginfunctionalitywithvalidcredentials(unittest.TestCase, LogInformation):
 
     def setUp(self):
+        self.TEST_INFORMATION(namefile=__class__.__name__, message="Class Name - " + __class__.__name__)
         self.driver = webdriver.Chrome()
         self.data = self.readyaml(__class__.__name__)
-        self.TEST_INFORMATION(namefile="abc", message="Class Name - " + __class__.__name__)
-        logging.basicConfig(filename=__class__.__name__+'.log', filemode='w',format='%(asctime)s - %(message)s', level=logging.INFO)
-        logging.info("Class Name - " + __class__.__name__)
 
     def readyaml(self, toFetchTestData):
         with open('yaml/config.yaml', 'r') as file:
             TestData = yaml.safe_load(file)
-
-            logging.basicConfig(filename=__class__.__name__+'.log', filemode='w',format='%(asctime)s - %(message)s', level=logging.INFO)
-            logging.info(TestData[toFetchTestData])
+            self.TEST_INFORMATION(namefile=__class__.__name__, message=TestData[toFetchTestData])
         return TestData[toFetchTestData]
 
     def test_check_login(self):
         driver = self.driver
         TestData = self.data
 
-        logging.basicConfig(filename=__class__.__name__+'.log', filemode='w',format='%(asctime)s - %(message)s', level=logging.INFO)
-        logging.info("url - " + TestData['url'])
+        self.TEST_INFORMATION(namefile=__class__.__name__, message="url - " + TestData['url'])
         driver.get(TestData['url'])
 
         self.assertIn("OrangeHRM", driver.title)
@@ -49,10 +44,10 @@ class Verifyloginfunctionalitywithvalidcredentials(unittest.TestCase, LogInforma
             logging.error("Timed out waiting for page to load")
 
         username_textbox = driver.find_element(By.NAME, "username")
-        logging.info("username - " + TestData['username'])
+        self.TEST_INFORMATION(namefile=__class__.__name__, message="username - " + TestData['username'])
         username_textbox.send_keys(TestData['username'])
 
-        logging.info("password - " + TestData['password'])
+        self.TEST_INFORMATION(namefile=__class__.__name__, message="password - " + TestData['password'])
         password_textbox = driver.find_element(By.NAME, "password")
         password_textbox.send_keys(TestData['password'])
 
@@ -64,12 +59,12 @@ class Verifyloginfunctionalitywithvalidcredentials(unittest.TestCase, LogInforma
             element_present = EC.presence_of_element_located((By.CSS_SELECTOR, "a.oxd-main-menu-item"))
             WebDriverWait(driver, timeout).until(element_present)
         except TimeoutException:
-            logging.error ("Timed out waiting for page to load")
+            self.TEST_INFORMATION(namefile=__class__.__name__, message="Timed out waiting for page to load")
 
         actualUrl=driver.current_url
         expectedUrl= TestData['expected']
-        logging.info("expectedUrl - " + expectedUrl)
-        logging.info("actualUrl - " + actualUrl)
+        self.TEST_INFORMATION(namefile=__class__.__name__, message="expectedUrl - " + expectedUrl)
+        self.TEST_INFORMATION(namefile=__class__.__name__, message="actualUrl - " + actualUrl)
         self.assertIn(expectedUrl, actualUrl)
 
 
--- 50_Most_Important/tools/logs.py
+++ 50_Most_Important/tools/logs.py
@@ -1,9 +1,11 @@
 import logging
+from datetime import datetime
 
 class LogInformation():
 
 	def TEST_INFORMATION(self,namefile="",message=""):
-		print(namefile)
-		namefile = "/Users/dheerajjain/Documents/Selenium/50_Most_Important/tools/"+namefile+".log"
-		logging.basicConfig(filename='namefile.log', filemode='w',format='%(asctime)s - %(message)s', level=logging.INFO)
+		#now = datetime.now()
+		#namefile = namefile+"-"+now.strftime("%d/%m/%Y-%H:%M:%S")+".log"
+		#print(namefile)
+		logging.basicConfig(filename=namefile+'.log', filemode='w',format='%(asctime)s - %(message)s', level=logging.INFO)
 		logging.info(message)
