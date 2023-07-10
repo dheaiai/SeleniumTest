a6d8008cf6fefa53b792c5c63a16d72f4c9f6716
dheaiai <dheerajjain60@gmail.com>
2023-07-10 10:27:00 +0200

    ForgotPassword added

--- 50_Most_Important/test_check_login.py
+++ 50_Most_Important/test_check_login.py
@@ -3,9 +3,7 @@ import unittest
 import yaml
 import time
 from tools.logs import LogInformation
-import logging
-#from logs import LogInformation
-#Selenium based
+#import logging
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -16,22 +14,25 @@ from selenium.webdriver.support import expected_conditions as EC
 
 class Verifyloginfunctionalitywithvalidcredentials(unittest.TestCase, LogInformation):
 
+    loggingData=""
+
     def setUp(self):
-        self.TEST_INFORMATION(namefile=__class__.__name__, message="Class Name - " + __class__.__name__)
+        self.loggingData = LogInformation(namefile=__class__.__name__)
+        self.loggingData.TEST_INFORMATION(namefile=__class__.__name__, message="Class Name - " + __class__.__name__)
         self.driver = webdriver.Chrome()
         self.data = self.readyaml(__class__.__name__)
 
     def readyaml(self, toFetchTestData):
         with open('yaml/config.yaml', 'r') as file:
             TestData = yaml.safe_load(file)
-            self.TEST_INFORMATION(namefile=__class__.__name__, message=TestData[toFetchTestData])
+            self.loggingData.TEST_INFORMATION(namefile=__class__.__name__, message=TestData[toFetchTestData])
         return TestData[toFetchTestData]
 
     def test_check_login(self):
         driver = self.driver
         TestData = self.data
 
-        self.TEST_INFORMATION(namefile=__class__.__name__, message="url - " + TestData['url'])
+        self.loggingData.TEST_INFORMATION(namefile=__class__.__name__, message="url - " + TestData['url'])
         driver.get(TestData['url'])
 
         self.assertIn("OrangeHRM", driver.title)
@@ -41,30 +42,32 @@ class Verifyloginfunctionalitywithvalidcredentials(unittest.TestCase, LogInforma
             element_present = EC.presence_of_element_located((By.NAME, "username"))
             WebDriverWait(driver, timeout).until(element_present)
         except TimeoutException:
-            logging.error("Timed out waiting for page to load")
+            self.loggingData.TEST_INFORMATION(namefile=__class__.__name__, message="Timed out waiting for page to load")
 
         username_textbox = driver.find_element(By.NAME, "username")
-        self.TEST_INFORMATION(namefile=__class__.__name__, message="username - " + TestData['username'])
+        self.loggingData.TEST_INFORMATION(namefile=__class__.__name__, message="username - " + TestData['username'])
         username_textbox.send_keys(TestData['username'])
 
-        self.TEST_INFORMATION(namefile=__class__.__name__, message="password - " + TestData['password'])
+        self.loggingData.TEST_INFORMATION(namefile=__class__.__name__, message="password - " + TestData['password'])
         password_textbox = driver.find_element(By.NAME, "password")
         password_textbox.send_keys(TestData['password'])
 
         SignIn_button = driver.find_element(By.CLASS_NAME, "oxd-button")
+        self.loggingData.TEST_INFORMATION(namefile=__class__.__name__, message="Press Submit Button")
         SignIn_button.submit()
 
         timeout = 10
         try:
+            self.loggingData.TEST_INFORMATION(namefile=__class__.__name__, message="Wait page to be load completely")
             element_present = EC.presence_of_element_located((By.CSS_SELECTOR, "a.oxd-main-menu-item"))
             WebDriverWait(driver, timeout).until(element_present)
         except TimeoutException:
-            self.TEST_INFORMATION(namefile=__class__.__name__, message="Timed out waiting for page to load")
+            self.loggingData.TEST_INFORMATION(namefile=__class__.__name__, message="Timed out waiting for page to load")
 
         actualUrl=driver.current_url
         expectedUrl= TestData['expected']
-        self.TEST_INFORMATION(namefile=__class__.__name__, message="expectedUrl - " + expectedUrl)
-        self.TEST_INFORMATION(namefile=__class__.__name__, message="actualUrl - " + actualUrl)
+        self.loggingData.TEST_INFORMATION(namefile=__class__.__name__, message="expectedUrl - " + expectedUrl)
+        self.loggingData.TEST_INFORMATION(namefile=__class__.__name__, message="actualUrl - " + actualUrl)
         self.assertIn(expectedUrl, actualUrl)
 
 
--- 50_Most_Important/test_check_login_failed.py
+++ 50_Most_Important/test_check_login_failed.py
@@ -4,8 +4,6 @@ import yaml
 import time
 from tools.logs import LogInformation
 import logging
-#from logs import LogInformation
-#Selenium based
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -16,22 +14,25 @@ from selenium.webdriver.support import expected_conditions as EC
 
 class Verifyloginfunctionalitywithinvalidcredentials(unittest.TestCase, LogInformation):
 
+    loggingData=""
+
     def setUp(self):
-        self.TEST_INFORMATION(namefile=__class__.__name__, message="Class Name - " + __class__.__name__)
+        self.loggingData = LogInformation(namefile=__class__.__name__)
+        self.loggingData.TEST_INFORMATION(namefile=__class__.__name__, message="Class Name - " + __class__.__name__)
         self.driver = webdriver.Chrome()
         self.data = self.readyaml(__class__.__name__)
 
     def readyaml(self, toFetchTestData):
         with open('yaml/config.yaml', 'r') as file:
             TestData = yaml.safe_load(file)
-            self.TEST_INFORMATION(namefile=__class__.__name__, message=TestData[toFetchTestData])
+            self.loggingData.TEST_INFORMATION(namefile=__class__.__name__, message=TestData[toFetchTestData])
         return TestData[toFetchTestData]
 
     def test_check_login_failed(self):
         driver = self.driver
         TestData = self.data
 
-        self.TEST_INFORMATION(namefile=__class__.__name__, message="url - " + TestData['url'])
+        self.loggingData.TEST_INFORMATION(namefile=__class__.__name__, message="url - " + TestData['url'])
         driver.get(TestData['url'])
 
         self.assertIn("OrangeHRM", driver.title)
@@ -44,10 +45,10 @@ class Verifyloginfunctionalitywithinvalidcredentials(unittest.TestCase, LogInfor
             logging.error("Timed out waiting for page to load")
 
         username_textbox = driver.find_element(By.NAME, "username")
-        self.TEST_INFORMATION(namefile=__class__.__name__, message="username - " + TestData['username'])
+        self.loggingData.TEST_INFORMATION(namefile=__class__.__name__, message="username - " + TestData['username'])
         username_textbox.send_keys(TestData['username'])
 
-        self.TEST_INFORMATION(namefile=__class__.__name__, message="password - " + TestData['password'])
+        self.loggingData.TEST_INFORMATION(namefile=__class__.__name__, message="password - " + TestData['password'])
         password_textbox = driver.find_element(By.NAME, "password")
         password_textbox.send_keys(TestData['password'])
 
@@ -59,20 +60,20 @@ class Verifyloginfunctionalitywithinvalidcredentials(unittest.TestCase, LogInfor
             element_present = EC.presence_of_element_located((By.CSS_SELECTOR, "p.oxd-text"))
             WebDriverWait(driver, timeout).until(element_present)
         except TimeoutException:
-            self.TEST_INFORMATION(namefile=__class__.__name__, message="Timed out waiting for page to load")
+            self.loggingData.TEST_INFORMATION(namefile=__class__.__name__, message="Timed out waiting for page to load")
 
         actualUrl=driver.current_url
         expectedUrl= TestData['expected']
-        self.TEST_INFORMATION(namefile=__class__.__name__, message="expectedUrl - " + expectedUrl)
-        self.TEST_INFORMATION(namefile=__class__.__name__, message="actualUrl - " + actualUrl)
+        self.loggingData.TEST_INFORMATION(namefile=__class__.__name__, message="expectedUrl - " + expectedUrl)
+        self.loggingData.TEST_INFORMATION(namefile=__class__.__name__, message="actualUrl - " + actualUrl)
         self.assertIn(expectedUrl, actualUrl)
 
         
         InvalidCredentials = driver.find_element(By.XPATH, '//*[text()="Invalid credentials"]')
         actualString=InvalidCredentials.text
         expectedString= TestData['expected2']
-        self.TEST_INFORMATION(namefile=__class__.__name__, message="expectedString - " + expectedString)
-        self.TEST_INFORMATION(namefile=__class__.__name__, message="actualString - " + actualString)
+        self.loggingData.TEST_INFORMATION(namefile=__class__.__name__, message="expectedString - " + expectedString)
+        self.loggingData.TEST_INFORMATION(namefile=__class__.__name__, message="actualString - " + actualString)
         self.assertIn(expectedString, actualString)
 
 
